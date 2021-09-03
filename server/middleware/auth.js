5a98519463091772810bec17962466987c6b05ed
nan-noo <54002105+nan-noo@users.noreply.github.com>
2021-09-03 22:24:37 +0900

    update login & register page using formik and yup
    add rememberMe & input error check

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,10 +1,10 @@
 const {User} = require('../models/User');
 
-let auth = (req, res, next) => {
+const auth = (req, res, next) => {
     // 인증 처리
     // get token from client cookie
     // decode token and find user ? okay : no!
-    let token = req.cookies.x_auth;
+    const token = req.cookies.x_auth;
 
     User.findByToken(token, (err, user) => {
         if(err) throw err;
@@ -16,8 +16,7 @@ let auth = (req, res, next) => {
         // 편의를 위해 저장
         req.token = token;
         req.user = user;
-
-        next(); // middleware이기 때문
+        next();
     });
 };
 
