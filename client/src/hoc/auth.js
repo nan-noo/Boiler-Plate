74743bb735c4e4eed8843019465b0d5f133f50b3
nan-noo <54002105+nan-noo@users.noreply.github.com>
2021-09-09 15:24:15 +0900

    delete console.log

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -11,7 +11,7 @@ export default function(SpecificComponent, option, adminRoute = null){
 
         useEffect(() => {
             dispatch(auth()).then(response => {
-                console.log(response);
+                //console.log(response);
                 if(!response.payload.isAuth){ // 로그인 안 한 상태
                     if(option){
                         props.history.push('/login');
